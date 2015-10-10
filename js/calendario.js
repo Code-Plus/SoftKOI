a562f74f4ee84a38cd408b087de1b284fffa827b
dragonizado <jesusfrancisco@hotmail.es>
2015-10-09 23:57:16 -0500

    fix de calendario
    el cuadro gris que se encuentra en el menu de dropdown en el calendario
    es porque se esta utilizando en el style.css una clase que afecta a
    todas la li en general eso causa ese bug en calendario emma por favor
    arreglar esa clase

--- js/calendario.js
+++ js/calendario.js
@@ -1,10 +1,11 @@
   $(document).ready(function() {
+
                 var date = new Date();
                 var d = date.getDate();
                 var m = date.getMonth();
                 var y = date.getFullYear();
 
-                var cId = $('#calendar'); 
+                var cId = $('#calendar');
 
                 //se crea el calendario
                 cId.fullCalendar({
@@ -94,43 +95,45 @@
                             className: 'bgm-dark'
                         },
                     ],
-                     
+
                     //On Day Select
                     select: function(start, end, allDay) {
-                        $('#addNew-event').modal('show');   
+                        $('#addNew-event').modal('show');
                         $('#addNew-event input:text').val('');
                         $('#getStart').val(start);
                         $('#getEnd').val(end);
                     }
                 });
 
-                //Create and ddd Action button with dropdown in Calendar header. 
-                var actionMenu = '<ul class="actions actions-alt" id="fc-actions">' +
+                //Create and ddd Action button with dropdown in Calendar header.
+                var actionMenu ='<ul class="actions actions-alt" id="fc-actions">' +
                                     '<li class="dropdown">' +
-                                        '<a href="" data-toggle="dropdown"><i class="ellipsis vertical icon"></i></a>' +
+                                        '<a href="#" id="dropdownm"><i class="ellipsis vertical icon"></i></a>' +
+                                        '<div class="ui dropdown">' +
                                         '<ul class="dropdown-menu dropdown-menu-right">' +
                                             '<li class="active">' +
-                                                '<a data-view="month" href="">Month View</a>' +
+                                                '<a data-view="month" href="">Ver Mes</a>' +
                                             '</li>' +
                                             '<li>' +
-                                                '<a data-view="basicWeek" href="">Week View</a>' +
+                                                '<a data-view="basicWeek" href="">Ver Semana</a>' +
                                             '</li>' +
                                             '<li>' +
-                                                '<a data-view="agendaWeek" href="">Agenda Week View</a>' +
+                                                '<a data-view="agendaWeek" href="">Agenda Vista Semanal</a>' +
                                             '</li>' +
                                             '<li>' +
-                                                '<a data-view="basicDay" href="">Day View</a>' +
+                                                '<a data-view="basicDay" href="">Ver por dias</a>' +
                                             '</li>' +
                                             '<li>' +
-                                                '<a data-view="agendaDay" href="">Agenda Day View</a>' +
+                                                '<a data-view="agendaDay" href="">Agenda Vista por dias</a>' +
                                             '</li>' +
                                         '</ul>' +
                                     '</div>' +
+                                    '</div>'
                                 '</li>';
 
 
                 cId.find('.fc-toolbar').append(actionMenu);
-                
+
                 //Event Tag Selector
                 (function(){
                     $('body').on('click', '.event-tag > span', function(){
@@ -138,12 +141,12 @@
                         $(this).addClass('selected');
                     });
                 })();
-            
+
                 //Add new Event
                 $('body').on('click', '#addEvent', function(){
                     var eventName = $('#eventName').val();
                     var tagColor = $('.event-tag > span.selected').attr('data-tag');
-                        
+
                     if (eventName != '') {
                         //Render Event
                         $('#calendar').fullCalendar('renderEvent',{
@@ -152,25 +155,25 @@
                             end:  $('#getEnd').val(),
                             allDay: true,
                             className: tagColor
-                            
+
                         },true ); //Stick the event
-                        
+
                         $('#addNew-event form')[0].reset()
                         $('#addNew-event').modal('hide');
                     }
-                    
+
                     else {
                         $('#eventName').closest('.form-group').addClass('has-error');
                     }
-                });   
+                });
 
                 //Calendar views
                 $('body').on('click', '#fc-actions [data-view]', function(e){
                     e.preventDefault();
                     var dataView = $(this).attr('data-view');
-                    
+
                     $('#fc-actions li').removeClass('active');
                     $(this).parent().addClass('active');
-                    cId.fullCalendar('changeView', dataView);  
+                    cId.fullCalendar('changeView', dataView);
                 });
-            });                        
+            });
