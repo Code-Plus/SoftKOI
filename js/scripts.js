2194db4d1abd39bf1c5bc06fe9b4df8dc87e7cce
maac4422 <miguelarenas98@hotmail.com>
2015-10-09 19:02:10 -0500

    Cambios perfil,consolas

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,23 @@ $(document).ready(function(){
   $('.ui.inline.dropdown')
    .dropdown();
 
-   $(".aceptar").click(function(){
-      $('.rescue2').modal('show');
-   });
+   
 
+   //Script de olvidar contraseña
    $(".olvidar").click(function(){
       $('.rescue').modal('show');
    });
-
+   //Script cuando confirma el envio de la contraseña olvidada
+  $(".aceptar").click(function(){
+      $('.rescue2').modal('show');
+   });
+  //Scipt de Modal cuando seleccione registrar
+        $(".registrar_alquiler").click(function(){
+          $('.registro_alquiler').modal('show');
+        });
+      //Script de Modal cuando seleccione aceptar el registro
+        $(".aceptar_alquiler").click(function(){
+          $('.registro_alquiler2').modal('show');
+        });
 
 });
