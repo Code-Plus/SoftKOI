3533cdc55b3bcc1459de681fe7f4534fe3b636a4
Jesus <jesusfrancisco@hotmail.es>
2015-11-11 14:42:27 -0500

    se completa el home aunque faltan algunos cambios como los colores de las graficas y tambien se estructura el footer

--- js/stadisticaa.js
+++ js/stadisticaa.js
@@ -1,4 +1,4 @@
-var iniciosAAA={
+var Dventas={
 	 labels : ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"],
 	datasets : [
 		{//primer conjunto de datos
@@ -19,6 +19,29 @@ var iniciosAAA={
 	]
 }
 
-var inicioAA = document.getElementById("inicioA").getContext("2d");
+var Reporte={
+	 labels : ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"],
+	datasets : [
+		{//primer conjunto de datos
+			fillColor:"rgb(0, 188, 212)",
+			strokeColor : "rgba(255,255,255,0.8)",
+			  highlightFill:"rgba(229, 137, 0,0.75)",
+			  highlightStroke:"rgba(229, 137,0,1)",
+			data :[10,20,30,40,50,40,5]
+		},
+		{//segundo conjunto de datos
+			fillColor:"rgba(151,147,205,0.5)",
+			strokeColor : "rgba(151,147,205,0.8)",
+			 highlightFill:"rgba(151,147,205,0.75)",
+			 highlightStroke:"rgba(151,147,205,1)",
+		 	data :[15,18,20,11,24,20,31]
+		}
 
-window.Bar = new Chart(inicioAA).Bar(iniciosAAA,{showScale: false,responsive : true});
+	]
+}
+
+var Gventas = document.getElementById("inicioA").getContext("2d");
+var Rep_SemPre_vs_Sempas = document.getElementById("inicioB").getContext("2d");
+
+window.Bar = new Chart(Gventas).Bar(Dventas,{showScale: true,responsive : true});
+window.Line = new Chart(Rep_SemPre_vs_Sempas).Line(Reporte,{showScale: true,responsive : true});
