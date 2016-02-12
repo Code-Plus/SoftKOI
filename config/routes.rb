36ddf906c693e11bda8dd34f65b7e3948e8a61c5
EmmanuelSW <edhgt89@hotmail.com>
2016-02-11 23:28:28 -0500

    Subiendo schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
+
 	devise_scope :user do
 		root to: 'devise/sessions#new' , :as => "login" 
-  	end
-=======
+  end
+
   resources :movies
   resources :clothings
   resources :consoles
   resources :genres
   resources :types_consoles
   resources :types_clothings
-  	root 'welcome#index'
-   devise_for :users
-  	get 'welcome/index'
->>>>>>> c95980a9594eaba21bd3252c6562d55a0fc88c0c
+
+
+  devise_for :users
 
 	get '/home', to: 'welcome#index', as: 'home'
 
-    devise_for :user	
 end
