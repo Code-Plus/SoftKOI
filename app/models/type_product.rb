b7755f39dd2a43acb9bf38603610f0dbd2316b48
Camila <mcamilaar10@gmail.com>
2016-02-15 14:52:17 -0500

    Validaciones tipo producto.

--- app/models/type_product.rb
+++ app/models/type_product.rb
@@ -1,5 +1,28 @@
 class TypeProduct < ActiveRecord::Base
 
+	#Gema para los estados.
+	include AASM
+
+	#Relaciones entre tablas.
 	has_many :categories
+
+	aasm column: "state" do 
+		#Estado por default
+		state :Disponible, :initial => true
+		state :NoDisponible
+
+		#Eventos de movimiento o transiciones para los estados.
+		event :Disponible do
+			transitions from: :NoDisponible, to: :Disponible
+		end
+
+		event :NoDisponible do
+			transitions from: :Disponible, to: :NoDisponible
+		end
+	end
+
+	#Validaciones 
+	validates :name, presence: true
+	validates :description, presence: true, length: { in: 8..80 }
 
 end
