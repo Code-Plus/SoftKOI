fd50d2c8e2ed60b3de78cd7f294f802b5a248061
EmmanuelSW <edhgt89@hotmail.com>
2016-02-18 13:42:54 -0500

    Seeds poblado de db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do |i|
+  	TypeProduct.create(name: "Type Product ##{i + 1}", description: "A product.", state: "disponible")
+  	Category.create(name: "Category ##{i + 1}", description: "A category", state: "disponible", type_product_id: i + 1)
+end
+
+1.times do |u|
+	User.create(document: "admin#{u}", encrypted_password: "123456789")
+end
