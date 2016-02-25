11ba631dece0f406f795b8b5d21795a6c237b2aa
EmmanuelSW <edhgt89@hotmail.com>
2016-02-25 14:11:49 -0500

    Productos y vistas

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.order('created_at DESC').page(params[:page]).per_page(5)
   end
 
   # GET /products/1
