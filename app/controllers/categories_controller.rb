33eb61b52c266fbc3f01ecd4e65a05d310d59415
EmmanuelSW <edhgt89@hotmail.com>
2016-03-02 22:35:46 -0500

    Modales categories listas

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,82 +1,80 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy, :noDisponible, :disponible]
 
-  def index
-    @categories = Category.order('created_at DESC').page(params[:page]).per_page(8)
-  end
+   before_action :set_category, only: [:show, :edit, :update, :destroy, :noDisponible, :disponible]
 
-  def show
-  end
+   def index
+      @categories = Category.order('created_at DESC').page(params[:page]).per_page(8)
+   end
 
-
-  def new
-    @category = Category.new
-    @type_products = TypeProduct.all 
-  end
+   def show
+   end
 
 
-  #En este metodo cambiaremos el estado a disponible de las categorias
-  def disponible
-    @category.disponible!
-    redirect_to @category
-  end
-  #En este metodo se cambia el estado a noDisponible de las categorias
-  def noDisponible
-    @category.noDisponible!
-    redirect_to @category
-  end
+   def new
+      @category = Category.new
+      @type_products = TypeProduct.all
+   end
 
 
-  def edit
-  end
+   def edit
+   end
 
 
-  def create
-    @category = Category.create(category_params)
+   def create
+      @category = Category.create(category_params)
 
-    respond_to do |format|
-      if @category.save
-        format.json { head :no_content }
-        format.js
-      else
-         format.json { render json: @category.errors.full_messages, 
-                            status: :unprocessable_entity }
+      respond_to do |format|
+         if @category.save
+            format.json { head :no_content }
+            format.js
+         else
+            format.json { render json: @category.errors.full_messages,
+               status: :unprocessable_entity }
+         end
       end
-    end
-  end
+   end
 
 
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.json { head :no_content }
-        format.js
-      else
-        format.json { render json: @category.errors.full_messages,
-                                   status: :unprocessable_entity }
+   def update
+      respond_to do |format|
+         if @category.update(category_params)
+            format.json { head :no_content }
+            format.js
+         else
+            format.json { render json: @category.errors.full_messages,
+               status: :unprocessable_entity }
+         end
       end
-    end
-  end
+   end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.json
-  def destroy
-    @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Categoria eliminada correctamente.' }
-      format.json { head :no_content }
-    end
-  end
+   def destroy
+      @category.destroy
+      respond_to do |format|
+         format.html { redirect_to categories_url, notice: 'Categoria eliminada correctamente.' }
+         format.json { head :no_content }
+      end
+   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
+   #Se cambia el estado a disponible de las categorias
+   def disponible
+      @category.disponible!
+      redirect_to @category
+   end
+
+   #Se cambia el estado a noDisponible de las categorias
+   def noDisponible
+      @category.noDisponible!
+      redirect_to @category
+   end
+
+   private
+
+   def set_category
       @category = Category.find(params[:id])
-    end
+   end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def category_params
-      #Se le mandan como parametros los campos de la base de datos de la tabla Category
+
+   def category_params
       params.require(:category).permit(:name, :description, :type_product_id)
-    end
+   end
 end
