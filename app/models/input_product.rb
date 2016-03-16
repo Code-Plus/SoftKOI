16963862e792a69f8c9c7f3c2d05548b1fc47109
EmmanuelSW <edhgt89@hotmail.com>
2016-03-16 10:18:01 -0500

    Actualizacion en input

--- app/models/input_product.rb
+++ app/models/input_product.rb
@@ -1,10 +1,9 @@
 class InputProduct < ActiveRecord::Base
 
-   belongs_to :product  
+   belongs_to :product
    validates :product_id, presence: true
    validates :stock, presence: true, numericality: {greater_than: 0}
    before_create :update_stock
-   
 
 
    #Getter de los valores de productos para el stock
@@ -12,14 +11,14 @@ class InputProduct < ActiveRecord::Base
       @product=value
    end
 
+
    private
-   
    #Actualizar el stock del producto al que se le hace la entrada
    def update_stock
       stock_product= product.stock
-      if self.stock.nil?
-      else 
+      unless self.stock.nil?
          product.update(stock: stock_product + self.stock)
       end
    end
+
 end
