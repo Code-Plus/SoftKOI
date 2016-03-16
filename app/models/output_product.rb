85032286cce8447046dcb2483c19dc2a94714771
EmmanuelSW <edhgt89@hotmail.com>
2016-03-16 12:24:57 -0500

    Refactoring

--- app/models/output_product.rb
+++ app/models/output_product.rb
@@ -3,7 +3,7 @@ class OutputProduct < ActiveRecord::Base
    belongs_to :product
    validates :product_id, presence: true
    validates :stock, presence: true, numericality: {greater_than: 0}
-   before_validation :update_stock  
+   before_validation :update_stock
 
 
    def product=(value)
@@ -18,14 +18,12 @@ class OutputProduct < ActiveRecord::Base
       if self.stock.nil?
 
       elsif (stock_product-self.stock)<0
-         self.errors.add(:stock ,"--->No hay suficientes productos para dar de baja")
+         self.errors.add(:base ,"No hay suficientes productos para dar de baja")
       else
-         
+
          product.update(stock: stock_product - self.stock)
       end
    end
-
-
 
 
 end
