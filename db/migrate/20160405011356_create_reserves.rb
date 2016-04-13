c62f896b89538155290de60f0bf9ac3ac8dba3b7
Camila <mcamilaar10@gmail.com>
2016-04-13 10:18:00 -0500

    Modificación campo base de datos (Quitar campo Producto en las vistas de Reserve)

--- db/migrate/20160405011356_create_reserves.rb
+++ db/migrate/20160405011356_create_reserves.rb
@@ -2,12 +2,11 @@ class CreateReserves < ActiveRecord::Migration
   def change
     create_table :reserves do |t|
       t.string :customer
-      t.integer :product_id
       t.date :date
       t.time :start_time
       t.time :end_time
       t.string :state
-      t.integer :price_reserve_id
+      t.integer :reserve_price_id
 
       t.timestamps null: false
     end
