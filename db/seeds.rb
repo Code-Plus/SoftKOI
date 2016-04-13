0b68a4a2e52aa3b6af0a31fb0e9fb9d39c98114c
EmmanuelSW <edhgt89@hotmail.com>
2016-04-13 11:31:09 -0500

    Poblado de la bd y nombre permiso

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 r1 = Role.create(:name => 'Administrador')
 r2 = Role.create(:name => 'Empleado')
+
+r1 = TypeDocument.create(:description => 'Cédula de ciudadania')
+r2 = TypeDocument.create(:description => 'Tarjeta de identidad')
 p "Created #{Role.count} types."
