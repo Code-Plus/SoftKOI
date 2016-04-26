fdf11be3302205c85c674bcce4909469ab4ef85a
EmmanuelSW <edhgt89@hotmail.com>
2016-04-25 20:04:17 -0500

    Corrigiendo ruta home con slug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
    layout "welcome"
 
    def index
+      redirect_to 'http://lh3.ggpht.com/-tn4-4SI_nwU/TjMac2JADfI/AAAAAAAAETY/FFoM2B5fhig/toll%252520face%252520%252520%2525286%252529_thumb.gif'
    end
 end
