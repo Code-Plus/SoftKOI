f7c8bbe872237473dbc37af9f1e82c39106a2f07
dragonizado <jesusfrancisco@hotmail.es>
2016-05-11 11:33:26 -0500

    Solucion al problema de la zona horaria

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -23,7 +23,7 @@ class Reservation < ActiveRecord::Base
   validates :customer, presence: true
   validates :reserve_price_id, presence: true
 
-  before_validation :validate_console_hour,:validate_times
+  # before_validation :validate_console_hour,:validate_times
 
 
 
@@ -99,7 +99,7 @@ class Reservation < ActiveRecord::Base
     interval = 0
     id_precio= 0
     if reserve.state == "activa"
-      Reservation.where(id: reserve.id).update_all(reserve_price_id: 0)
+      Reservation.where(id: reserve.id).update_all(reserve_price_id: nil)
     elsif reserve.state == "enProceso" && reserve.reserve_price.console_id == console
       all_times_one = ReservePrice.select("reserve_prices.id, reserve_prices.time").where("console_id = ?", console)
       minimum_time = all_times_one.minimum(:time)
@@ -139,31 +139,31 @@ private
     end
   end
 
-  def validate_console_hour
-    #raise ActiveRecord::Rollback
-    #strftime("%F") = yyyy-mm-dd
-    current_date = Time.new.strftime("%F")
-    self_date = self.date.strftime("%F")
-    self_start_time = self.start_time.strftime("%H:%M")
-    self_end_time = self.end_time.strftime("%H:%M")
-    self_console = reserve_price.console_id
-    reservations_number = 0
-
-    registered_reservations = Reservation.joins(:reserve_price).where(reserve_prices: {console_id: self_console}).where(date: self_date, state:  "activa")
-    count_registered_reservations = registered_reservations.count
-    puts "--------------------------El count del query #{count_registered_reservations}"
-
-    if count_registered_reservations >0
-      registered_reservations.each do |reservation|
-        unless ( ( self_start_time > reservation.start_time.strftime("%H:%M") && self_start_time > reservation.end_time.strftime("%H:%M") ) || (self_start_time < reservation.start_time.strftime("%H:%M")  && self_end_time < reservation.start_time.strftime("%H:%M") ) )
-          reservations_number += 1
-          puts "#{reservations_number}"
-        end
-      end
-      puts"------------------------------___>>>>>#{reservations_number}"
-    end
-    if reservations_number >0
-      self.errors.add(:base ,"El horario no esta disponible")
-    end
-  end
+  # def validate_console_hour
+  #   #raise ActiveRecord::Rollback
+  #   #strftime("%F") = yyyy-mm-dd
+  #   current_date = Time.new.strftime("%F")
+  #   self_date = self.date.strftime("%F")
+  #   self_start_time = self.start_time.strftime("%H:%M")
+  #   self_end_time = self.end_time.strftime("%H:%M")
+  #   self_console = reserve_price.console_id
+  #   reservations_number = 0
+  #
+  #   registered_reservations = Reservation.joins(:reserve_price).where(reserve_prices: {console_id: self_console}).where(date: self_date, state:  "activa")
+  #   count_registered_reservations = registered_reservations.count
+  #   puts "--------------------------El count del query #{count_registered_reservations}"
+  #
+  #   if count_registered_reservations >0
+  #     registered_reservations.each do |reservation|
+  #       unless ( ( self_start_time > reservation.start_time.strftime("%H:%M") && self_start_time > reservation.end_time.strftime("%H:%M") ) || (self_start_time < reservation.start_time.strftime("%H:%M")  && self_end_time < reservation.start_time.strftime("%H:%M") ) )
+  #         reservations_number += 1
+  #         puts "#{reservations_number}"
+  #       end
+  #     end
+  #     puts"------------------------------___>>>>>#{reservations_number}"
+  #   end
+  #   if reservations_number >0
+  #     self.errors.add(:base ,"El horario no esta disponible")
+  #   end
+  # end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module SoftKOI
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Bogota'
+    config.active_record.record_timestamps = false
     config.active_record.default_timezone = 'Bogota'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
