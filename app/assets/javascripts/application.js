ff41ffbac1a782a17f05349ada787d8a293eebed
EmmanuelSW <edhgt89@hotmail.com>
2016-05-12 15:26:40 -0500

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,6 +29,36 @@
 //= require turbolinks-c
 //= require_tree .
 
+//Datatable
+function load_datatable(){
+  $('#datatable').DataTable({
+    "language": {
+      "sProcessing":     "Procesando...",
+      "sLengthMenu":     "Mostrar  _MENU_  registros",
+      "sZeroRecords":    "No se encontraron resultados",
+      "sEmptyTable":     "Ningún dato disponible en esta tabla",
+      "sInfo":           "_TOTAL_ registros en total.",
+      "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+      "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+      "sInfoPostFix":    "",
+      "sSearch":         "Buscar:",
+      "sUrl":            "",
+      "sInfoThousands":  ",",
+      "sLoadingRecords": "Cargando...",
+      "oPaginate": {
+        "sFirst":    "Primero",
+        "sLast":     "Último",
+        "sNext":     "Siguiente &nbsp;<i class='fa fa-angle-right'></i>",
+        "sPrevious": "<i class='fa fa-angle-left'></i>&nbsp; Anterior"
+      },
+      "oAria": {
+        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+      }
+    }
+  });
+}
+
 toastr.options = {
   "positionClass": "toast-bottom-right",
   "closeButton": true,
