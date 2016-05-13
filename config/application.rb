c2182eefe684dc12916e9aaedc14b66b44cc5c4c
Camila <mcamilaar10@gmail.com>
2016-05-12 20:55:33 -0500

    Algoritmo de los cinco minutos de la reserva

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,11 @@ module SoftKOI
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Bogota'
-    # config.active_record.record_timestamps = false
-    # config.active_record.default_timezone = 'Bogota'
+    config.time_zone = "Bogota"
+    config.active_record.default_timezone = :local
+    config.active_record.record_timestamps = false
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
