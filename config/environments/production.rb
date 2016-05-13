4b56ffdda3a96df40ca0bf69be78ed17aa44e934
EmmanuelSW <edhgt89@hotmail.com>
2016-05-13 08:58:18 -0500

    fix error 500

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.delivery_method = :smtp
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.action_mailer.default_url_options = { :host => 'https://softkoi-dev.herokuapp.com' }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
