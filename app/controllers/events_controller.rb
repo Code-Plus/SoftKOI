535f03e87e5db63bdb7734f0c40c6a0afb50a345
EmmanuelSW <edhgt89@hotmail.com>
2016-05-16 10:38:05 -0500

    Configurando calendario

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,31 +1,28 @@
 class EventsController < ApplicationController
+  
   before_action :set_event, only: [:show, :edit, :update, :destroy]
   after_action :day_of_event, only:[:index]
   # after_action :day_of_event, only:[:create]
 
-  # GET /events
-  # GET /events.json
+
   def index
     @events = Event.all
     @event = Event.new
   end
 
-  # GET /events/1
-  # GET /events/1.json
+
   def show
   end
 
-  # GET /events/new
   def new
     @event = Event.new
   end
 
-  # GET /events/1/edit
+
   def edit
   end
 
-  # POST /events
-  # POST /events.json
+
   def create
     @event = Event.new(event_params)
 
@@ -39,6 +36,7 @@ class EventsController < ApplicationController
       end
     end
   end
+
 
   def create_event
     @event = Event.create(event_params)
@@ -61,8 +59,7 @@ class EventsController < ApplicationController
      end
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
+
   def update
     respond_to do |format|
       if @event.update(event_params)
@@ -75,23 +72,22 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
+
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      format.html { redirect_to events_url, notice: 'El evento ha sido eliminado correctamente.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_event
       @event = Event.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def event_params
       params.require(:event).permit(:description, :start_time, :name)
     end
