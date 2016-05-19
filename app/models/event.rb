225d616186f9a8535a3a837957bbc351b8d95bdc
maac4422 <miguelarenas98@hotmail.com>
2016-05-19 10:16:23 -0500

    Corrigiendo error en agenda

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,7 @@ class Event < ActiveRecord::Base
   before_update :set_updated_at
 
   validates :description, presence: true
-  validates_date :start_time, presence: true, :on_or_after => lambda { Date.current } ,:on_or_after_message => " no debe ser menor a la actual"
+  validates_datetime :start_time, presence: true, :on_or_after => lambda { Date.current } ,:on_or_after_message => " no debe ser menor a la actual"
   validates :name, presence: true
 
 
