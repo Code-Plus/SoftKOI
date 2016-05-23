d8b42b13c2512d4355c5eb127f4606fad5447f21
dragonizado <jesusfrancisco@hotmail.es>
2016-05-23 08:52:42 -0500

    Validando la existencia del producto en cambios

--- app/assets/javascripts/cupons.js
+++ app/assets/javascripts/cupons.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
   $('#change_register').click(function(){
-    // var products_id = $('.info_product_id').val();
-    // var products_quantity = $('.info_product_quantity').val();
-    // alert("estos son los ids de los productos "+ products_id);
-    // alert("estos son las cantidades de los productos" + products_quantity);
+    
     $('#frm_coupons_products').submit();
   });
 });
