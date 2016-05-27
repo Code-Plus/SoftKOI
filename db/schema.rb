ddb9324ef51be6d0d7ea89c0b597ec19a91442c5
dragonizado <jesusfrancisco@hotmail.es>
2016-05-27 02:09:11 -0500

    actualizadon despues de formatear

--- db/schema.rb
+++ db/schema.rb
@@ -203,7 +203,7 @@ ActiveRecord::Schema.define(version: 20160523133124) do
     t.integer  "amount",       default: 0
     t.integer  "total_amount", default: 0
     t.integer  "discount",     default: 0
-    t.date     "limit_date",   default: '2016-05-26'
+    t.date     "limit_date",   default: '2016-05-27'
     t.text     "comment"
     t.integer  "user_id"
     t.integer  "customer_id"
