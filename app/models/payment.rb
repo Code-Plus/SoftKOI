10c621bee117faeb19ccb3fdfb97a1cf5f1d5dc8
maac4422 <miguelarenas98@hotmail.com>
2016-06-13 12:49:58 -0500

    Validacion que un cliente no pueda deber mas de 50000  y que si es menor de edad no pueda tener deudas

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -7,7 +7,6 @@ class Payment < ActiveRecord::Base
   before_create :set_date
   after_create :validate_amount
   before_update :set_updated_at
-  before_validation :validate_due, :validate_age
 
 
   private
@@ -43,32 +42,4 @@ class Payment < ActiveRecord::Base
     end
   end
 
-  #Validar que no deba mas de 50.000
-  def validate_due
-    customer_due = 0
-    customer_pay = 0
-    sales_id = sale.id
-    customer_info = sale.customer_id
-    query_sale = Sale.where(customer_id: customer_info)
-    query_sale.each do |sale|
-      customer_due += sale.total_amount
-      query_payment = Payment.where(sale_id: sale.id)
-      query_payment.each do |pay|
-        customer_pay += pay.amount
-      end
-    end
-    if customer_due - customer_pay > 50000
-      self.errors.add(:base ,"Ha exedido el limite de prestamo.")
-    end
-  end
-
-  #Validar que un menor de edad no deba
-  def validate_age
-    customer_age = sale.customer.age
-    if customer_age < 18
-      unless self.amount == sale.amount
-        self.errors.add(:base ,"Es menor de edad, no esta permitido darle prestamos.")
-      end
-    end
-  end
 end
